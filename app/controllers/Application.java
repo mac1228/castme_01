ce7d5ae1d84cc7d9a2980bbdb06dbb2884f30e04
Michael Curd <mac1228@gmail.com>
2013-09-17 18:10:26 -0700

    modified routes for selection

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,27 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    	return ok(views.html.index.render("Hello World"));
+    }
+    
+    public static Result actors() {
+        return TODO;
+    }
+    
+    public static Result directors() {
+        return TODO;
+    }
+    
+    public static Result studio() {
+        return TODO;
+    }
+    
+    public static Result genre() {
+        return TODO;
+    }
+    
+    public static Result releaseDate() {
+        return TODO;
     }
   
 }
